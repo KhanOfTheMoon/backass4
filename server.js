1673c6fabcdb01d069c7546961bab05acfe9a7ed
KhanOfTheMoon <aibek03062007@gmail.com>
2026-01-29 14:41:57 +0500

    first commit

---
+++ server.js
@@ -0,0 +1,37 @@
+const express = require("express");
+const path = require("path");
+require("dotenv").config();
+
+const connectDB = require("./config/db");
+const authRoutes = require("./routes/authRoutes");
+const bookRoutes = require("./routes/bookRoutes");
+const reviewRoutes = require("./routes/reviewRoutes");
+const errorHandler = require("./middleware/errorHandler");
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(express.static(path.join(__dirname, "local")));
+app.use("/local", express.static(path.join(__dirname, "local")));
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "local", "index.html"));
+});
+
+app.use("/api/auth", authRoutes);
+app.use("/api/books", bookRoutes);
+app.use("/api/reviews", reviewRoutes);
+
+app.use((req, res) => {
+  res.status(404).json({ error: "Route not found" });
+});
+
+app.use(errorHandler);
+
+(async () => {
+  await connectDB();
+  const PORT = process.env.PORT || 3000;
+  app.listen(PORT, () => console.log(`Server running at http://localhost:${PORT}`));
+})();
